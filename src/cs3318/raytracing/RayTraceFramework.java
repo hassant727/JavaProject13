aa24346f00f3655e3dd36f0272a2a5f6ea639928
Tariq <hmdtr@a483e79d5f68.ant.amazon.com>
2020-12-05 22:03:44 +0000

    create branch

--- src/cs3318/raytracing/RayTraceFramework.java
+++ src/cs3318/raytracing/RayTraceFramework.java
@@ -26,10 +26,6 @@ import java.util.List;
 class Vector3D {
     public float x, y, z;
 
-    // constructors
-    public Vector3D( ) {
-    }
-
     public Vector3D(float x, float y, float z) {
         this.x = x; this.y = y; this.z = z;
     }
